c9c48798d4cecb6262b8883da806b4aee5b280a9
ParkGeongi <pgg113579@naver.com>
2022-10-06 17:52:26 +0900

    2022-10-30

--- for_loop_test/for_loop.py
+++ for_loop_test/for_loop.py
@@ -11,3 +11,6 @@ def solution():
 
 if __name__ == "__main__":
     solution()
+    
+
+
--- 탐욕/화폐교환.py
+++ 탐욕/화폐교환.py
@@ -3,35 +3,27 @@ class Solution:
       title = " ### 화폐교환 ###"
       aster = "*"*30
       answer = f"요청금액 : {money} 원"
-      
       print (f'{title} \n {aster} \n {answer} \n {aster}')
-      
-      unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
 
+      unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10]   
+      
       for i in unit:
          count = money // i
          money = money % i
-         print(f'{i}원짜리 {count}개')
+         print(f'{i}원: {count}매')
 
 
-      #answer2 =  money // unit[0]
-      #answer3 = (money % unit[0]) // unit[1]
-      #answer4 = (money % unit[1]) // unit[2]
-     # answer5 = (money % unit[2])  // unit[3]
-      #answer6 = (money % unit[3])  // unit[4]
-      #answer7 = (money % unit[4])  // unit[5]
-      #answer8 = (money % unit[5])  // unit[6]
-      #answer9 = (money % unit[6])  // unit[7]
+
       return (
-       #f'{title} \n {aster} \n {answer} \n {aster}\n '
-       #f'5만원 : {answer2}매 \n '
-       #f'1만원 : {answer3}매 \n '
-       #f'5천원 : {answer4}매 \n '
-       #f'1천원 : {answer5}매 \n '
-       #f'5백원 : {answer6}매 \n '
-       #f'1백원 : {answer7}매 \n ' 
-       #f'5십원 : {answer8}매 \n ' 
-       #f'1십원 : {answer9}매'
+       ''' #f'{title} \n {aster} \n {answer} \n {aster}\n '
+       f'5만원 : {answer2}매 \n '
+       f'1만원 : {answer3}매 \n '
+       f'5천원 : {answer4}매 \n '
+       f'1천원 : {answer5}매 \n '
+       f'5백원 : {answer6}매 \n '
+       f'1백원 : {answer7}매 \n ' 
+       f'5십원 : {answer8}매 \n ' 
+       f'1십원 : {answer9}매'    '''
    )    
 
 if __name__=="__main__":
